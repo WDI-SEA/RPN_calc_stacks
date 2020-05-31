a741b12f87012d567057bae305df9c23b733d50c
gjones81 <jones.guy81@gmail.com>
2020-05-31 15:33:20 -0700

    Working

--- solution.py
+++ solution.py
@@ -5,51 +5,76 @@ def calculate_rpn(input):
     split_input = input.split()
     print(split_input)
     # def recursive_calculate(split_input):
-    #     if len(split_input) <= 1:
+    #     if len(split_input) == 1:
     #         return split_input
     #     else:
     for i in range(len(split_input)):
-        print(split_input, 'line 9')
-        if split_input[i] not in operators:
-            bin.append(split_input[i])
+        new_input = split_input
+        print(new_input, 'line 9')
+        if new_input[i] not in operators:
+            bin.append(new_input[i])
             print(bin, 'line 12')
         else:
-            print(split_input[i], 'line 14')
-            num2 = int(bin.pop(i - 1))
-            num1 = int(bin.pop(i -2))
+            print(new_input[i], 'line 14')
+            num2 = int(bin.pop())
+            num1 = int(bin.pop(-1))
             print(num1)
             print(num2)
-            if split_input[i] == '+':
+            if new_input[i] == '+':
                 result = num1 + num2
                 print(result)
                 bin.append(str(result))
                 print(bin, 'line 22')
-                continue
                 # recursive_calculate(bin)
-            elif split_input[i] == '-':
+            elif new_input[i] == '-':
                 result = num1 - num2
                 print(result)
                 bin.append(str(result))
                 print(bin, 'line 29')
-                continue
-                #recursive_calculate(bin)
-            elif split_input[i] == '/':
+                # recursive_calculate(bin)
+            elif new_input[i] == '/':
                 result = num1 / num2
                 print(str(result))
                 bin.append(str(result))
                 print(bin, 'line 35')
-                continue
-                #recursive_calculate(bin)
+                # recursive_calculate(bin)
             else:
                 result = num1 * num2
                 print(result)
                 bin.append(str(result))
                 print(bin, 'line 41')
-                continue
-                #recursive_calculate(bin)
-            
+                # recursive_calculate(bin)
             
 
-print(calculate_rpn('2 2 3 4 * *'))
+print(calculate_rpn('2 4 +')) 
+print(calculate_rpn('2')) 
+print(calculate_rpn('2 1 -')) 
+print(calculate_rpn('12 4 *')) 
+print(calculate_rpn('12 381 + 111 -')) 
+print(calculate_rpn('1 2 3 + 4 * +'))        
+# Basic Test
+# if calculate_rpn('2 4 +') == 6: print('✅ Passing for "2 4 +"')
+# else: print('🚫 Not passing for "2 4 +"')
+
+# # Test for single number as input
+# if calculate_rpn('2') == 2: print('✅ Passing for "2"')
+# else: print('🚫 Not passing for "2"')
+
+# # Test for subtraction
+# if calculate_rpn('2 1 -') == 1: print('✅ Passing for "2 1 -"')
+# else: print('🚫 Not passing for "2 1 -"')
+
+# # Test for 2-digit numbers
+# if calculate_rpn('12 4 *') == 48: print('✅ Passing for "12 4 *"')
+# else: print('🚫 Not passing for "12 4 *"')
+
+# # Test multiple calculations
+# if calculate_rpn('12 381 + 111 -') == 282: print('✅ Passing for "12 381 + 111 -"')
+# else: print('🚫 Not passing for "12 381 + 111 -"')
+
+# # Test multiple calculations
+# if calculate_rpn('1 2 3 + 4 * +') == 21: print('✅ Passing for "1 2 3 + 4 * +"')
+# else: print('🚫 Not passing for "1 2 3 + 4 * +"')
+
 
 
