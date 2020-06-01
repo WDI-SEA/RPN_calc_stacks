f3f2b1ed44d4077db652a3af17ea257b9c53a674
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-06-01 10:04:59 -0700

    created class stack and converted to stack method also hit some bonuses

--- solution.py
+++ solution.py
@@ -3,12 +3,28 @@ ops = {
   "+": (lambda a, b: a + b),
   "-": (lambda a, b: a - b),
   "*": (lambda a, b: a * b),
-  "/": (lambda a, b: a / b)
+  "/": (lambda a, b: a / b),
+  "**": (lambda a, b: a ** b)
 }
+class Stack:
+  def __init__(self):
+    self.list = []
+
+  def pop(self):
+    return self.list.pop()
+
+  def push(self, data):
+    self.list.append(data)
+
+  def length(self):
+    return len(self.list)
+
+  def print_stack(self):
+    print([str(x) for x in self.list])
 
 def calculate_rpn(input):
   # Declare an empty stack
-  stack = []
+  stack = Stack()
   # Split the string input on spaces
   input = input.split(' ')
   # Iterate through the input
@@ -16,13 +32,13 @@ def calculate_rpn(input):
     # Check if the item is an operator or not
     if i in ops:
       # If operator: Solve for the operator with the last two numbers (Two pops, then a push)
-      arg2 = stack.pop()
-      arg1 = stack.pop()
-      result = ops[i](arg1, arg2)
-      stack.append(result)
+      num2 = stack.pop()
+      num1 = stack.pop()
+      result = ops[i](num1, num2)
+      stack.push(result)
     else:
  # If number: Save for later (Push to stack)
-      stack.append(int(i))
+      stack.push(int(i))
   # return the one item remaining on the stack
   return stack.pop()
 
@@ -40,8 +56,8 @@ if calculate_rpn('2 1 -') == 1: print('✅ Passing for "2 1 -"')
 else: print('🚫 Not passing for "2 1 -"')
 
 # Test for 2-digit numbers
-if calculate_rpn('12 4 *') == 48: print('✅ Passing for "12 4 *"')
-else: print('🚫 Not passing for "12 4 *"')
+if calculate_rpn('12 4 **') == 20736: print('✅ Passing for "12 4 **"')
+else: print('🚫 Not passing for "12 4 **"')
 
 # Test multiple calculations
 if calculate_rpn('12 381 + 111 -') == 282: print('✅ Passing for "12 381 + 111 -"')
